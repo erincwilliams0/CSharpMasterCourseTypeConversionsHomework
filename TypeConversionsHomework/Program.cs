f47f953cdf02a63e479e773a7eef42e5d5a4e6c0
erin williams <erin.cwilliams0@gmail.com>
2025-05-14 18:52:47 -0500

    Add project files.

---
+++ TypeConversionsHomework/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TypeConversionsHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // generic greeting
+            // capture the users age 
+            // validate the users age
+            //	make sure a number was provided
+            //	make sure they are a realistic age 1 through 110
+            // store the users age in 25 years
+            // store what the users age would have been 25 years ago, negatives are acceptable
+            // print out message in the format... you would be ... inn 25 years, and 25 years ago you were ...
+        }
+    }
+}
